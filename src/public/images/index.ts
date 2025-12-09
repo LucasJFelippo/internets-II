1a2a0d0c38755e1c0cb19e10b29f7b6667f6dec3
Beatriz-SMB <bsnbernardors@gmail.com>
2025-12-09 10:32:41 -0300

    feat: add new image imports for icons and update export statement

--- src/public/images/index.ts
+++ src/public/images/index.ts
@@ -1,3 +1,4 @@
+import logo from ".//logo.png";
 import aboutSpotify from "./about-spotify.jpg";
 import ceoImage from "./ceo-image.jpg";
 import headerLogo from "./header-logo.png";
@@ -6,4 +7,23 @@ import spotifyLoudClear from "./spotify-loud-clear.jpg";
 import sustainabilityReport from "./sustainability-report.jpg";
 import timeToPlayFair from "./time-to-play-fair.png";
 
-export { aboutSpotify, ceoImage, headerLogo, lifeAtSpotify, spotifyLoudClear, sustainabilityReport, timeToPlayFair };
+// icons
+import artists from "./artists.svg";
+import creators from "./creators.svg";
+import advertisers from "./advertisers.svg";
+import lifeAt from "./life-at.svg";
+import design from "./design.svg";
+import engineering from "./engineering.svg";
+import vendors from "./vendors.svg";
+import songwriters from "./songwriters.svg";
+import inventors from "./inventors.svg";
+import help from "./help.svg";
+import spotify from "./Spotify.svg";
+
+import instagram from "./instagram.svg";
+import X from "./X.svg";
+import facebook from "./facebook.svg";
+
+export { logo, aboutSpotify, ceoImage, headerLogo, lifeAtSpotify, spotifyLoudClear, sustainabilityReport, timeToPlayFair,
+            artists, creators, advertisers, lifeAt, design, engineering, vendors, songwriters, inventors, help, spotify,
+            instagram, X, facebook };
