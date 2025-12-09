5a5ed32bf84e813bf271e1aa552b965aa1306fd5
Beatriz-SMB <bsnbernardors@gmail.com>
2025-12-09 11:19:23 -0300

    feat: add header component with logo, navigation links, and search functionality

---
+++ src/components/Header.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import { ChevronDown } from "lucide-react";
+import { Search } from "lucide-react";
+import { headerLogo } from "@/public/images";
+
+const Header = () => {
+    return (
+        <header className="w-full bg-black h-20 flex justify-between px-12 items-center fixed z-99">    
+            <div className="flex gap-20">
+            <div className="flex gap-4 items-center">
+                <Image
+                src={headerLogo}
+                alt="Spotify Logo"
+                width={46}
+                height={46}
+                />
+                <h2 className="text-[22px] font-extrabold">For the Record</h2>
+            </div>
+            <ul className="flex items-center gap-8">
+                <li className="flex gap-1">
+                    <a href="https://www.spotify.com/br/about-us/contact/" className="font-bold" target="_blank" rel="noopener noreferrer">News</a>
+                    <ChevronDown />
+                </li>
+                <li className="flex gap-1">
+                    <a href="https://www.spotify.com/br/about-us/press/" className="font-bold" target="_blank" rel="noopener noreferrer">Company</a>
+                    <ChevronDown />
+                </li>
+            </ul>  
+            </div>
+            <div className="flex items-end border-b-2 border-solid w-122 pb-2">
+            <input type="text" id="search" placeholder="Search..." className="w-full outline-0 text-2xl font-bold placeholder:text-white"/>
+            <Search />
+            </div>
+
+            {/* <button id="theme-toggle" className="theme-toggle-button">Mudar Tema</button>     */}
+        </header>
+    );
+};
+
+export default Header;
