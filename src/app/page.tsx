2484c02660dfd3390b7539b0cf704fb408cc8a8c
Beatriz-SMB <bsnbernardors@gmail.com>
2025-12-08 23:06:18 -0300

    feat: update home page with new sections and images for sustainability and company initiatives

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { headerLogo, aboutSpotify, ceoImage, lifeAtSpotify } from "@/public/imagens";
+import { headerLogo, aboutSpotify, ceoImage, lifeAtSpotify, sustainabilityReport, timeToPlayFair, spotifyLoudClear } from "@/public/imagens";
 import Image from "next/image";
 import { ChevronDown } from "lucide-react";
 import { Search } from "lucide-react";
@@ -18,17 +18,17 @@ export default function Home() {
             <h2 className="text-[22px] font-extrabold">For the Record</h2>
           </div>
           <ul className="flex items-center gap-8">
-            <li className="flex">
+            <li className="flex gap-1">
+                <a href="https://www.spotify.com/br/about-us/contact/" className="font-bold" target="_blank" rel="noopener noreferrer">News</a>
                 <ChevronDown />
-                <a href="https://www.spotify.com/br/about-us/contact/" target="_blank" rel="noopener noreferrer">News</a>
             </li>
-            <li className="flex">
+            <li className="flex gap-1">
+                <a href="https://www.spotify.com/br/about-us/press/" className="font-bold" target="_blank" rel="noopener noreferrer">Company</a>
                 <ChevronDown />
-                <a href="https://www.spotify.com/br/about-us/press/" target="_blank" rel="noopener noreferrer">Company</a>
             </li>
         </ul>  
         </div>
-        <div className="flex items-end border-b-2 border-solid w-90 pb-2">
+        <div className="flex items-end border-b-2 border-solid w-122 pb-2">
           <input type="text" id="search" placeholder="Search..." className="w-full outline-0 text-2xl font-bold placeholder:text-white"/>
           <Search />
         </div>
@@ -36,7 +36,7 @@ export default function Home() {
         {/* <button id="theme-toggle" className="theme-toggle-button">Mudar Tema</button>     */}
       </header>
       <section className="flex border-b-2 border-solid border-black">
-        <div className="w-[65%] border-r-2 border-solid border-black px-20 py-10">
+        <div className="w-[66.67%] border-r-2 border-solid border-black px-20 py-10">
           <Image src={aboutSpotify} alt="About Spotify" className="w-full object-cover"/>
           <h1 className="text-black capitalize font-bold text-9xl my-6">about spotify</h1>
           <p className="text-black">
@@ -52,20 +52,48 @@ export default function Home() {
             180 markets.
           </p>
         </div>
-        <div className="w-[35%]">
-          <div className="border-b-2 border-solid border-black p-12 h-140 ">
+        <div className="w-[33.3%]">
+          <div className="group border-b-2 border-solid border-black p-12 h-140 hover:bg-[#2D46B9]">
               <Image src={ceoImage} alt="CEO Image" className="w-full object-cover"/>
-              <h3 className="uppercase text-[#1ED760] font-bold my-3">a message from our ceo, daniel ek</h3>
-              <p className="text-black text-justify">Spotify Founder and CEO Daniel Ek Discusses the Economics of Music Streaming</p>
+              <h3 className="uppercase text-[#1ED760] font-bold my-3 group-hover:text-white">a message from our ceo, daniel ek</h3>
+              <p className="text-black text-justify cursor-pointer group-hover:text-white">Spotify Founder and CEO Daniel Ek Discusses the Economics of Music Streaming</p>
           </div>
           <div className="group flex flex-col items-center h-140 px-12 pt-24 relative hover:bg-[#2D46B9]">
               <h2 className="text-black font-bold text-6xl absolute top-12.5 group-hover:text-white">Life at Spotify</h2>
               <Image src={lifeAtSpotify} alt="Life at Spotify" className="w-full object-cover"/>
-              <button className="bg-[#1ED760] text-black px-8 py-3 mt-8 rounded-full font-bold uppercase hover:bg-white">learn more</button>
+              <button className="bg-[#1ED760] text-black px-8 py-3 mt-8 rounded-full font-bold uppercase cursor-pointer hover:bg-white hover:border">learn more</button>
           </div>
         </div>
       </section>
-      <>teste</>
+      <section className="flex border-b-2 border-black">
+        <div className="w-[66.67%] p-24 bg-[#c3f0c8] border-r-2 border-black">
+          <p className="text-black font-bold text-3xl leading-12">
+            Our mission is to unlock the potential of human creativity — by giving a million creative artists the opportunity 
+            to live off their art and billions of fans the opportunity to enjoy and be inspired by it.
+          </p>
+        </div>
+        <div className="w-[33.33%] bg-black flex flex-col justify-center items-center px-18">
+          <h2 className="text-white text-6xl font-bold leading-18">By the Numbers</h2>
+          <button className="bg-black text-[#1ED760] border border-[#1ED760] rounded-full px-7 py-3 text-[14px] font-bold mt-10 cursor-pointer uppercase hover:bg-[#1ED760] hover:text-black">spotify inventors</button>
+        </div>
+      </section>
+      <section className="flex">
+        <div className="w-[34%] p-14">
+          <Image src={sustainabilityReport} alt="Sustainability Report" className="w-full object-cover"/>
+          <h4 className="text-black text-3xl font-bold my-6">Sustainability Report</h4>
+          <p className="text-black">Lowering emissions isn’t something we do on the side. It’s an integrated part of everyday business. Learn how we’re taking action.</p>
+        </div>
+        <div className="w-[34%] p-14 border-x-2 border-solid border-black">
+          <Image src={timeToPlayFair} alt="Time to Play Fair" className="w-full object-cover"/>
+          <h4 className="text-black text-3xl font-bold my-6">Time to Play Fair</h4>
+          <p className="text-black">When competition is fair, both consumers and companies win. Learn about our efforts to even the playing field for all developers.</p>
+        </div>
+        <div className="w-[34%] p-14">
+          <Image src={spotifyLoudClear} alt="Spotify Loud & Clear" className="w-full object-cover"/>
+          <h4 className="text-black text-3xl font-bold my-6">Spotify Loud & Clear</h4>
+          <p className="text-black">Artists deserve clarity about the economics of music streaming. This site sheds light on the global streaming economy and royalty system.</p>
+        </div>
+      </section>
     </main>
   );
 }
