d1c785460bef35cf0afe34a04ab9e9e60cf99ff2
Beatriz-SMB <bsnbernardors@gmail.com>
2025-12-09 08:56:43 -0300

    feat: remove unused image assets from public directory

--- src/public/imagens/index.ts
+++
@@ -1,9 +0,0 @@
-import aboutSpotify from "./about-spotify.jpg";
-import ceoImage from "./ceo-image.jpg";
-import headerLogo from "./header-logo.png";
-import lifeAtSpotify from "./life-at-spotify.svg";
-import spotifyLoudClear from "./spotify-loud-clear.jpg";
-import sustainabilityReport from "./sustainability-report.jpg";
-import timeToPlayFair from "./time-to-play-fair.png";
-
-export { aboutSpotify, ceoImage, headerLogo, lifeAtSpotify, spotifyLoudClear, sustainabilityReport, timeToPlayFair };
