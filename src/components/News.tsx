445e9cff8716909a440f310e6d7b7cad64d1b2e4
Beatriz-SMB <bsnbernardors@gmail.com>
2025-12-12 15:09:20 -0300

    refactor: remove control prop from News component and utilize global context for active section

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -1,11 +1,20 @@
+"use client";
 import Image from "next/image";
 import { X } from "lucide-react";
 import { audiobooks, companyNews, spotifyLoudClear, wrapped2025 } from "@/public/images";
-import { ControlType } from "@/types/global";
+import { useGlobal } from "@/context/GlobalContext";
+import { useRouter } from "next/navigation";
 
-const News = ({ control }: ControlType) => {
+const News = () => {
 
-    const { setActiveSection } = control;
+    const router = useRouter();
+
+    const { setActiveSection } = useGlobal();
+
+    const handleClick = () => {
+        setActiveSection(null);
+        router.push("/news");
+    };
 
     return (
         <div className="w-full h-screen flex justify-between bg-black border-t-2 border-white relative py-18 px-24">
@@ -13,13 +22,13 @@ const News = ({ control }: ControlType) => {
                 <X size={30} />
             </span>
             <div>
-                <ul className="flex flex-col gap-6">
+                <ul className="flex flex-col gap-6 mb-12">
                     <li className="capitalize font-bold text-4xl cursor-pointer">news & announcements</li>
                     <li className="capitalize font-bold text-4xl cursor-pointer">artists & creators</li>
                     <li className="capitalize font-bold text-4xl cursor-pointer">culture & trends</li>
                     <li className="capitalize font-bold text-4xl cursor-pointer">behind the business</li>
                 </ul>
-                <button className="bg-black text-white font-bold uppercase border border-white px-6 py-3 rounded-full mt-12 hover:bg-white hover:text-black">view all news</button>
+                 <button className="bg-black text-white font-bold uppercase border border-white px-6 py-3 rounded-full mt-12 hover:bg-white hover:text-black" onClick={() => handleClick()}>view all news</button>
             </div>
             <div>
                 <h3 className="font-extrabold mb-4 text-1xl">Featured categories</h3>
