ba74dc6dc0156970afd4949be7e631ab1747dff0
Beatriz-SMB <bsnbernardors@gmail.com>
2025-12-12 15:09:29 -0300

    refactor: redefine GlobalContextType to manage active section state

--- src/types/global.ts
+++ src/types/global.ts
@@ -1,5 +1,5 @@
-export type ControlType = {
-  control: {
-    setActiveSection: (value: "news" | "company" | null) => void;
-  };
+export type GlobalContextType = {
+  activeSection: "news" | "company" | null;
+  setActiveSection: React.Dispatch<React.SetStateAction<"news" | "company" | null>>;
 };
+
