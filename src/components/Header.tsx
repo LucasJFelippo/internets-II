e58ae816b08fcc972dab75cb8052f9089458c238
Dandrielx <dionysandriel@gmail.com>
2025-12-19 09:46:12 -0300

    Link to news

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ import { useGlobal } from "@/context/GlobalContext";
 
 const Header = () => {
 
-     const { setActiveSection } = useGlobal();
+    const { setActiveSection } = useGlobal();
 
     return (
         <header className="w-full bg-black h-20 flex justify-between px-12 items-center fixed z-99">
@@ -26,9 +26,11 @@ const Header = () => {
                     </Link>
                 </div>
                 <ul className="flex items-center gap-8">
-                    <li className="flex gap-1 cursor-pointer" onClick={() => setActiveSection("news")}>
-                        <span>News</span>
-                        <ChevronDown />
+                    <li className="flex gap-1 cursor-pointer">
+                        <Link href="/news" className="flex items-center gap-1">
+                            <span>News</span>
+                            <ChevronDown />
+                        </Link>
                     </li>
                     <li className="flex gap-1 cursor-pointer" onClick={() => setActiveSection("company")}>
                         <span>Company</span>
@@ -37,8 +39,8 @@ const Header = () => {
                 </ul>
             </div>
             <div className="flex items-end border-b-2 border-solid w-122 pb-2">
-                    <input type="text" id="search" placeholder="Search..." className="w-full outline-0 text-2xl font-bold placeholder:text-white"  />
-                    <Search />
+                <input type="text" id="search" placeholder="Search..." className="w-full outline-0 text-2xl font-bold placeholder:text-white" />
+                <Search />
             </div>
 
             {/* <button id="theme-toggle" className="theme-toggle-button">Mudar Tema</button>     */}
